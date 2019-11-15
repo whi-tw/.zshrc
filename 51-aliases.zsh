81bc7c90473984db57100fd74f1efb957ff60e72
Tom Whitwell <tom@whi.tw>
2019-11-15 21:48:15 +0000

    change gitmodule thing to push and pull

--- 51-aliases.zsh
+++ 51-aliases.zsh
@@ -2,7 +2,7 @@ alias rm="rm -v"
 alias cp="cp -v"
 alias mv="mv -v"
 
-function update-gitstuff {
+function pull_config {
     REPOS=("${HOME}/.zshrc.d" "${HOME}/.password-store")
     for repo in "${REPOS[@]}"
     do
@@ -11,4 +11,14 @@ function update-gitstuff {
         popd
     done
     yadm pull --rebase
+}
+function push_config {
+    REPOS=("${HOME}/.zshrc.d" "${HOME}/.password-store")
+    for repo in "${REPOS[@]}"
+    do
+        pushd "${repo}"
+        git push
+        popd
+    done
+    yadm push
 }

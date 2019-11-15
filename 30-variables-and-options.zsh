ff6c255ac86a0d5a4f6c5216ce275af7026b40a9
Tom Whitwell <tom.whitwell@digital.cabinet-office.gov.uk>
2019-11-15 13:33:32 +0000

    GO FAST

--- 30-variables-and-options.zsh
+++ 30-variables-and-options.zsh
@@ -12,7 +12,6 @@ setopt auto_pushd
 setopt hash_list_all            # Hash everything before completion
 setopt completealiases          # Complete alisases
 setopt always_to_end            # When completing from the middle of a word, move the cursor to the end of the word
-setopt correct                  # Spelling correction for commands
 setopt list_ambiguous           # Complete as much of a completion until it gets ambiguous.
 setopt append_history
 setopt auto_menu
@@ -25,8 +24,6 @@ setopt chase_links              # Resolve symlinks
 
 zstyle ':completion:*' menu select
 zstyle ':completion:*' matcher-list '' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
-
-# CASE_SENSITIVE="true"
 
 # History
 export HISTFILE="$HOME/.zsh_history"
--- 96-zplugin-loading-plugins.zsh
+++ 96-zplugin-loading-plugins.zsh
@@ -5,41 +5,26 @@
 zplugin ice wait'0' lucid
 zplugin light zdharma/z-p-submods
 
-zplugin ice atclone"gdircolors -b LS_COLORS > clrs.zsh" atpull'%atclone' pick"clrs.zsh"
-zplugin load trapd00r/LS_COLORS
+zplugin ice wait'1' lucid \
+    atclone"gdircolors -b LS_COLORS > clrs.zsh" \
+    atpull'%atclone' pick"clrs.zsh"
+zplugin light trapd00r/LS_COLORS
 alias ls='gls --color=always'   # To enable the coloring on ls
 
 zstyle ':completion:*:default' list-colors \
-       ${(s.:.)LS_COLORS} # To enable the coloring on completion
+    ${(s.:.)LS_COLORS} # To enable the coloring on completion
 
-# Dependent installation of fzf to work
-# Check above for better interactive cd utility function
-zplugin light changyuheng/zsh-interactive-cd
 
 zplugin ice wait'0' atload'_zsh_autosuggest_start' lucid
 zplugin light zsh-users/zsh-autosuggestions
 export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=23'
 
-zplugin ice atclone"./libexec/pyenv init - > zpyenv.zsh" \
+zplugin ice wait'1' lucid atclone"./libexec/pyenv init - > zpyenv.zsh" \
     atinit'export PYENV_ROOT="${HOME}/.pyenv"' atpull"%atclone" \
     as'command' pick'bin/pyenv' src"zpyenv.zsh" nocompile'!'
 zplugin light pyenv/pyenv
 
-zplugin ice from"gh-r" as"program" mv"direnv* -> direnv" \
-    atclone'./direnv hook zsh > zhook.zsh' atpull'%atclone'\
-    pick"direnv" src"zhook.zsh"
-zplugin light direnv/direnv
-
-zplugin ice as"program" from"${GITHUB_USERNAME}@github.com"  mv"gds* -> gds" \
-    pick"gds" make"gds"
-zplugin light alphagov/gds-cli
-
-zplugin ice from"github" as"program" mv"mykman -> ykman" \
-    pick"ykman"
-zplugin light whi-tw/mykman
-
-zplugin ice atclone'brew install thefuck && thefuck --alias >  zhook.zsh' atpull'%atclone'\
-     src"zhook.zsh"
-zplugin light nvbn/thefuck
+zplugin ice wait'1' lucid
+zplugin light laggardkernel/zsh-thefuck
 
 zplugin light "chrissicool/zsh-256color"

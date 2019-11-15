833d983b657a588f4015ad689d902b79e92c6aff
Tom Whitwell <tom@whi.tw>
2019-11-15 21:45:34 +0000

    add gitstuff update function

--- 51-aliases.zsh
+++ 51-aliases.zsh
@@ -1,3 +1,14 @@
 alias rm="rm -v"
 alias cp="cp -v"
 alias mv="mv -v"
+
+function update-gitstuff {
+    REPOS=("${HOME}/.zshrc.d" "${HOME}/.password-store")
+    for repo in "${REPOS[@]}"
+    do
+        pushd "${repo}"
+        git pull --rebase
+        popd
+    done
+    yadm pull --rebase
+}

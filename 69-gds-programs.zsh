681b87f082ceab0437a8ca0ec1fb3594f4a3d9b9
Tom Whitwell <tom@whi.tw>
2019-11-24 15:07:03 +0000

    start working towards downloading releases
    from private repo

--- 69-gds-programs.zsh
+++ 69-gds-programs.zsh
@@ -6,4 +6,7 @@ zplugin ice wait"0" lucid as"program" from"${GITHUB_USERNAME}@github.com" mv"gds
 	make"!gds" if'[[ -n "$commands[go]" ]] && ! [[ -z "${GITHUB_USERNAME}" ]]' \
 	atclone'./gds bash-completion > completion.sh' atpull'%atclone' \
 	pick"gds" src'completion.sh'
+# zplugin ice wait"0" as"program" from"${GITHUB_USERNAME}:${GITHUB_API_TOKEN}@gh-r-api" mv"gds* -> gds" \
+# 	atclone'./gds bash-completion > completion.sh' atpull'%atclone' \
+# 	pick"gds" src'completion.sh'
 zplugin light alphagov/gds-cli

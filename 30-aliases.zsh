4fbae7d5ec97ba47e357efdffae00c82b671fdac
Tom Whitwell <tom@whi.tw>
2019-12-17 22:46:01 +0000

    alias grep to colorise

--- 30-aliases.zsh
+++ 30-aliases.zsh
@@ -2,4 +2,8 @@ alias rm="rm -v"
 alias cp="cp -v"
 alias mv="mv -v"
 
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+
 alias delim="seq  -f "=" -s '' `tput cols`"

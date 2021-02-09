fea4cd287e56cb7a4a1a1b6d2f305146d695dcf3
Tom Whitwell <tom@whi.tw>
2021-02-09 15:23:41 +0000

    reenable the non-systemd ssh-agent config
    Use my fork of oh-my-zsh's ssh-agent plugin

--- 50-agents.zsh
+++ 50-agents.zsh
@@ -1,9 +1,12 @@
 # Agents
-#zinit ice wait"0" lucid \
-#	if'[[ -f "${HOME}/.ssh/config" ]] && [[ ! -S "${XDG_RUNTIME_DIR}/ssh-agent.socket" ]]'
-#zinit snippet OMZ::plugins/ssh-agent/ssh-agent.plugin.zsh
-
-export SSH_AUTH_SOCK="${XDG_RUNTIME_DIR}/ssh-agent.socket"
+if [[ -f "${HOME}/.ssh/config" ]] && [[ ! -f "${HOME}/.config/systemd/user/ssh-agent.service" ]]; then
+	zstyle :omz:plugins:ssh-agent identities id_2_ed25519 id_ecdsa id_rsa id_rsa_2048
+	zstyle :omz:plugins:ssh-agent identity_dir .ssh2
+	zinit ice wait"0" lucid
+	zinit snippet https://raw.githubusercontent.com/whi-tw/oh-my-zsh/master/plugins/ssh-agent/ssh-agent.plugin.zsh
+else
+	export SSH_AUTH_SOCK="${XDG_RUNTIME_DIR}/ssh-agent.socket"
+fi
 
 zinit ice wait"0" lucid \
 	if'[[ ! -S "${XDG_RUNTIME_DIR}/gnupg/S.gpg-agent" ]]'

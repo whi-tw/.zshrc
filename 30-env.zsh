9a310cd2c238e6ce491881d53da014fcdd5c8f24
Tom Whitwell <tom@whi.tw>
2021-03-08 09:28:25 +0000

    remove BROWSER doodad - will be set properly by manjaro now

--- 30-env.zsh
+++ 30-env.zsh
@@ -2,4 +2,4 @@ export RSHELL_EDITOR="code -rw"
 
 #export AWS_VAULT_BACKEND="kwallet"
 
-test ! -z "$commands[chromium]" && export BROWSER="$(which chromium)"
+#test ! -z "$commands[chromium]" && export BROWSER="$(which chromium)"

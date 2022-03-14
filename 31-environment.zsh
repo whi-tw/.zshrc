8d0988c41f36b8de1f557a883d4afe8cf00b25a8
Tom Whitwell <tom@whi.tw>
2022-03-14 10:06:41 +0000

    switch ssh envars around and make them elif'y

--- 31-environment.zsh
+++ 31-environment.zsh
@@ -3,18 +3,19 @@ if [[ -n "${commands[nano]}" ]] && [[ "${TERM_PROGRAM}" != "vscode" ]]; then
     export EDITOR="${VISUAL}"
 fi
 
-if [[ -f "${HOME}/.config/systemd/user/ssh-add.service" ]]; then
+if [[ -n "${commands[ssh-askpass]}" ]]; then
+    export SSH_ASKPASS="${commands[ssh-askpass]}"
+fi
+
+if [[ -e "${HOME}/.1password/agent.sock" ]]; then
+    export SSH_AUTH_SOCK="${HOME}/.1password/agent.sock"
+elif [[ -e "${HOME}/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock" ]]; then
+    export SSH_AUTH_SOCK="${HOME}/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+elif [[ -f "${HOME}/.config/systemd/user/ssh-add.service" ]]; then
     export SSH_AUTH_SOCK="${XDG_RUNTIME_DIR}/ssh-agent.socket"
     export SSH_ASKPASS="/usr/bin/ksshaskpass"
 fi
-if [[ -e "${HOME}/.1password/agent.sock" ]]; then
-    export SSH_AUTH_SOCK="${HOME}/.1password/agent.sock"
-    unset SSH_ASKPASS
-fi
-if [[ -e "${HOME}/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock" ]]; then
-    export SSH_AUTH_SOCK="${HOME}/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
-    unset SSH_ASKPASS
-fi
+
 if [[ -n "${commands[kwallet-query]}" ]]; then
     export AWS_VAULT_BACKEND="kwallet"
 fi

82cd2e245df16711814fe0d010083282911c0191
Tom Whitwell <tom@whi.tw>
2022-05-12 10:43:17 +0100

    add completion for `gh` and alias `rm` to trash - need to stop `rm`ing stuff and hoping it will magically not be gone forever

--- 83-znap-function.zsh
+++ 83-znap-function.zsh
@@ -4,3 +4,6 @@ compctl -K    _pip_completion pip
 znap function _python_argcomplete pipx  'eval "$( register-python-argcomplete pipx  )"'
 complete -o nospace -o default -o bashdefault \
            -F _python_argcomplete pipx
+
+znap function _gh gh 'eval "$(gh completion -s zsh)"'
+compdef _gh gh
--- os-specific/mac.zsh
+++ os-specific/mac.zsh
@@ -3,3 +3,5 @@ test -d "${HOME}/.local/iTerm2-shell-integration/utilities" \
     && export PATH="${HOME}/.local/iTerm2-shell-integration/utilities:${PATH}"
 test -d "${HOME}/.local/jetbrains-bin" \
     && export PATH="${HOME}/.local/jetbrains-bin:${PATH}"
+
+test -n "${commands[trash]}" && alias rm="${commands[trash]}"

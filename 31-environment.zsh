ea70b6dd7173914a163797cd2c7e13af1cbf0bf3
Tom Whitwell <tom@whi.tw>
2022-12-18 10:35:38 +0000

    prefer systemd ssh-agent over 1password

--- 31-environment.zsh
+++ 31-environment.zsh
@@ -7,13 +7,12 @@ if [[ -n "${commands[ssh-askpass]}" ]]; then
     export SSH_ASKPASS="${commands[ssh-askpass]}"
 fi
 
-if [[ -e "${HOME}/.1password/agent.sock" ]]; then
+if [[ -e "${XDG_RUNTIME_DIR}/ssh-agent.socket" ]]; then
+    export SSH_AUTH_SOCK="${XDG_RUNTIME_DIR}/ssh-agent.socket"
+elif [[ -e "${HOME}/.1password/agent.sock" ]]; then
     export SSH_AUTH_SOCK="${HOME}/.1password/agent.sock"
 elif [[ -e "${HOME}/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock" ]]; then
     export SSH_AUTH_SOCK="${HOME}/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
-elif [[ -f "${HOME}/.config/systemd/user/ssh-add.service" ]]; then
-    export SSH_AUTH_SOCK="${XDG_RUNTIME_DIR}/ssh-agent.socket"
-    export SSH_ASKPASS="/usr/bin/ksshaskpass"
 fi
 
 if [[ -n "${commands[kwallet-query]}" ]]; then

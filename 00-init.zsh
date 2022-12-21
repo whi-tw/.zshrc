9c016b49db210b7ef49dd8048e16b1e315951b7e
Tom Whitwell <tom@whi.tw>
2022-12-21 11:41:48 +0000

    order files in the output as I intended
    create zsh cache dir before anything else

--- 00-init.zsh
+++ 00-init.zsh
@@ -1,3 +1,13 @@
+if [[ -z "$ZSH_CACHE_DIR" ]]; then
+    if [[ -n "${XDG_CACHE_HOME}" ]]; then
+        ZSH_CACHE_DIR="${XDG_CACHE_HOME}/.zsh"
+    else
+        ZSH_CACHE_DIR="${HOME}/.cache/.zsh"
+    fi
+fi
+
+[[ -d "${ZSH_CACHE_DIR}" ]] || mkdir -p "${ZSH_CACHE_DIR}"
+
 [[ -f ~/.znap/zsh-snap/znap.zsh ]] ||
     git clone --depth 1 -- \
         https://github.com/marlonrichert/zsh-snap.git ~/.znap/zsh-snap
@@ -9,13 +19,3 @@ source ~/.znap/zsh-snap/znap.zsh
     git clone --branch v0.9.0 -- \
         https://github.com/asdf-vm/asdf.git ~/.asdf
 source $HOME/.asdf/asdf.sh
-
-if [[ -z "$ZSH_CACHE_DIR" ]]; then
-    if [[ -n "${XDG_CACHE_HOME}" ]]; then
-        ZSH_CACHE_DIR="${XDG_CACHE_HOME}/.zsh"
-    else
-        ZSH_CACHE_DIR="${HOME}/.cache/.zsh"
-    fi
-fi
-
-[[ -d "${ZSH_CACHE_DIR}" ]] || mkdir -p "${ZSH_CACHE_DIR}"

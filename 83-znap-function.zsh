b6d0ac84d9b7e36c422a049f817600e2ff7b2d6b
Tom Whitwell <tom@whi.tw>
2022-12-28 13:49:15 +0000

    add poetry completion

--- 83-znap-function.zsh
+++ 83-znap-function.zsh
@@ -4,3 +4,6 @@ compctl -K    _pip_completion pip
 znap function _python_argcomplete pipx  'eval "$( register-python-argcomplete pipx  )"'
 complete -o nospace -o default -o bashdefault \
            -F _python_argcomplete pipx
+
+znap function _poetry poetry            'eval "$(poetry completions zsh --alias poetry | sed "s/_poetry_.*_complete/_poetry/g")"'
+compdef _poetry poetry

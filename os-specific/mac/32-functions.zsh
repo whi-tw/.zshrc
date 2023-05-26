3ce3017a7122ba71ba7efc0eacae5fdb1aef364e
Tom Whitwell <tom@whi.tw>
2023-05-26 09:59:08 +0100

    Update `sudo` function
    Rather than creating a file in /tmp to avoid the grep on subsequent
    calls, instead undeclare the function - this means the function
    won't be called again in the same session.
    
    Also, add an explainer comment to the function.

--- os-specific/mac/32-functions.zsh
+++ os-specific/mac/32-functions.zsh
@@ -1,11 +1,16 @@
 function sudo() {
-  test -f '/tmp/sudo_pam_tid_set' || {
-    grep -qs pam_tid.so /etc/pam.d/sudo || {
-      echo "pam_tid.so has been removed. Adding it back in..." >&2
-      builtin command sudo sed -i -e '2s;^;auth       sufficient     pam_tid.so\n;' /etc/pam.d/sudo
-      echo "resuming normal service..." >&2
-    }
-    touch /tmp/sudo_pam_tid_set
+  # I like using touchid for sudo. You can enable it by adding `pam_tid.so` to /etc/pam.d/sudo.
+  # MacOS reverts `/etc/pam.d/sudo` after updates because it hates you.
+  # This function:
+  # 1. checks if pam_tid.so is in /etc/pam.d/sudo
+  #    - if not, it adds it back in on the second line (ie. after the header comment)
+  # 2. undeclares itself, so it doesn't get called again in this session
+  # 3. calls sudo with the original arguments
+  grep -qs pam_tid.so /etc/pam.d/sudo || {
+    echo "pam_tid.so has been removed. Adding it back in..." >&2
+    builtin command sudo sed -i -e '2s;^;auth       sufficient     pam_tid.so\n;' /etc/pam.d/sudo
+    echo "resuming normal service..." >&2
   }
-  builtin command sudo "$@"
+  unset -f sudo
+  sudo "$@"
 }

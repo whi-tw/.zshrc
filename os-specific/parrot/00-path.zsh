292df54b4dafd91f6527e778054f62a5b6aba21f
Tom Whitwell <tom@whi.tw>
2023-07-20 16:36:25 +0100

    parrot changes

--- os-specific/parrot/00-path.zsh
+++ os-specific/parrot/00-path.zsh
@@ -1,2 +1,1 @@
-## get the path right
-export PATH=~/.local/bin:/snap/bin:/usr/sandbox/:/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games:/usr/share/games:/usr/local/sbin:/usr/sbin:/sbin:${PATH}
+export PATH="${HOME}/.local/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games:/usr/share/games:/usr/local/sbin:/usr/sbin:/sbin:/snap/bin"

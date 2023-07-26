5eb4ba7fcbde9c690f92a47bd9db763d10d97fce
Tom Whitwell <tom@whi.tw>
2023-07-26 13:46:35 +0100

    remove unnecessary p10k plugins

--- .profiling/999-profile-suffix.zsh
+++ .profiling/999-profile-suffix.zsh
@@ -1,3 +1,6 @@
-# Description: Prints profiling information after .zshrc has finished.
-
-zprof >/tmp/zprof_$(date +%Y-%m-%dT%H:%M:%S%z)
+# Description: Writes profiling information after .zshrc has finished.
+__outfile=/tmp/zprof_$(date +%Y-%m-%dT%H:%M:%S%z)
+zprof >"${__outfile}"
+print -s cat "${__outfile}"
+unset __outfile
+zprof -c

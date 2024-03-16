9d6b4531fe9207982404969e439409b39f606a35
Tom Whitwell <tom@whi.tw>
2024-03-16 10:33:41 +0000

    explicitely set zoxide cmd to z

--- 81-znap-eval.zsh
+++ 81-znap-eval.zsh
@@ -1,4 +1,4 @@
 (( $+commands[mcfly] ))   && znap eval mcfly 'mcfly init zsh'
 (( $+commands[thefuck] )) && znap eval thefuck 'thefuck --alias'
 (( $+commands[direnv] ))  && znap eval direnv_hook 'direnv hook zsh'
-(( $+commands[zoxide] ))  && znap eval zoxide 'zoxide init zsh'
+(( $+commands[zoxide] ))  && znap eval zoxide 'zoxide init --cmd=z zsh'

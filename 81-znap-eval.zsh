1fb3003c8748eab7823204439718b7e312bdc79c
Tom Whitwell <tom@whi.tw>
2024-05-24 09:58:40 +0100

    add aws-vault completion script

--- 81-znap-eval.zsh
+++ 81-znap-eval.zsh
@@ -2,3 +2,5 @@
 (( $+commands[thefuck] )) && znap eval thefuck 'thefuck --alias'
 (( $+commands[direnv] ))  && znap eval direnv_hook 'direnv hook zsh'
 (( $+commands[zoxide] ))  && znap eval zoxide 'zoxide init --cmd=z zsh'
+
+(( $+commands[aws-vault] )) && znap eval aws-vault 'curl -fsSL https://github.com/99designs/aws-vault/raw/master/contrib/completions/zsh/aws-vault.zsh'

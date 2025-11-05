52f31409c608b051c8d2633ff5f6149bf1de7cfb
Tom Whitwell <tom@whi.tw>
2025-11-05 11:00:24 +0000

    use local mise if available

--- 02-tool-management.zsh
+++ 02-tool-management.zsh
@@ -1,11 +1,15 @@
-zinit as="command" lucid from="gh-r" for \
+(( ! $+commands[usage] )) && zinit as="command" lucid from="gh-r" for \
     id-as="usage" \
     atpull="%atclone" \
     jdx/usage
 
-zinit as="command" lucid from="gh-r" for \
-    id-as="mise" mv="mise* -> mise" \
-    atclone="./mise* completion zsh > _mise" \
-    atpull="%atclone" \
-    atload='eval "$(mise activate zsh)"' \
-    jdx/mise
+if [[ -z ${commands[mise]} ]]; then
+    zinit as="command" lucid from="gh-r" for \
+        id-as="mise" mv="mise* -> mise" \
+        atclone="./mise* completion zsh > _mise" \
+        atpull="%atclone" \
+        atload='eval "$(mise activate zsh)"' \
+        jdx/mise
+else
+    eval "$(mise activate zsh)"
+fi
